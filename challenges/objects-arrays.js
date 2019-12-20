5f5263107bc099afa0aad8228fcc198de404879a
FinalBoss <finalb@projectfinalboss.com>
2019-12-20 12:29:52 -0500

    complete some arrays, skipping for later.  moving onto functions

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -89,8 +89,15 @@ const graduates = [
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array. This will be an array of strings.
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
-const universities = [];
-console.log(universities);
+
+const universities = graduates.map((university) => {
+  return {'university': university.university}
+})
+
+universities.sort()
+
+console.log(universities)
+
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
 
@@ -98,13 +105,20 @@ The resulting contact information strings should have a space between the first 
 "Josh josh@example.com"
 
 Log the result of your new array. */
-const contactInfo = [];
+const contactInfo = graduates.map((first_name) => {
+
+  return {'first-name': first_name.email}
+
+})
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
-const unisWithUni = [];
-console.log(unisWithUni);
 
+const unisWithUni = graduates.filter((univerity) => {
+  return univerity >= 'Uni';
+});
+
+console.log(unisWithUni);
 
 // ==== ADVANCED Array Methods ====
 
@@ -128,7 +142,11 @@ const zooAnimals = [
 The zoos want to display both the scientific name and the animal name in front of the habitats. Populate the displayNames array with only the animal_name and scientific_name of each animal. displayNames will be an array of strings, and each string should follow this pattern: "Name: Jackal, asiatic, Scientific: Canis aureus."
 
 */
-const displayNames = [];
+const displayNames = zooAnimals.forEach((animal_name) => {
+  
+  return animal_name >= animal_name.scientific_name
+});
+
 console.log(displayNames);
 
 /* Request 2: .map()
